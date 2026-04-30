9b6a87aff63cbc568ecc9155071e797a422dd11c
masud-softvence <masudrana@softvence.com>
2026-04-30 11:37:18 +0600

    enterprise plan system done

---
+++ prisma/migrations/20260430040411_remove/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `industryOther` on the `EnterprisePlanProposal` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "EnterprisePlanProposal" DROP COLUMN "industryOther";
